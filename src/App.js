c62164ce2cdf6acefe5eefc764658bebeb6d5507
olimov Amir <olimov.amir@mail.ru>
2024-02-08 13:34:29 +0300

    created component header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Header/>
       </header>
+      <div>
+        <aside>
+
+        </aside>
+        <main>
+
+        </main>
+      </div>
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
