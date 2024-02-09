01b84954748ebf01bc9d6649f369632a2e859f23
olimov Amir <olimov.amir@mail.ru>
2024-02-09 21:19:11 +0300

    has been created file Documents.jsx

--- src/pages/Documents.jsx
+++ src/pages/Documents.jsx
@@ -6,109 +6,76 @@ import fotoDisigner2 from '../images/2.jpg';
 import fotoDisigner3 from '../images/3.jpg';
 import fotoDisigner4 from '../images/4.jpg';
 import fotoDisigner5 from '../images/5.jpg';
+import CashExpense from './CashExpense';
 
 function Documents() {
-  return (
-    <>
-      <TabContainer id='ledt-tabs-example' defaultActiveKey='first'>
-        <Row>
-          <Col sm={2}>
-            <Nav variant='pills' className='flex-column mt-2'>
-              <NavItem>
-                <Nav.Link eventKey='first' className="nav-link" style={{ textAlign: 'left' }}>
-                  Design
-                </Nav.Link>
-              </NavItem>
-              <NavItem>
-                <Nav.Link eventKey='second' className="nav-link" style={{ textAlign: 'left' }}>
-                  Team
-                </Nav.Link>
-              </NavItem>
-              <NavItem>
-                <Nav.Link eventKey='third' className="nav-link" style={{ textAlign: 'left' }}>
-                  Programming
-                </Nav.Link>
-              </NavItem>
-              <NavItem>
-                <Nav.Link eventKey='fourth' className="nav-link" style={{ textAlign: 'left' }}>
-                  Design
-                </Nav.Link>
-              </NavItem>
-              <NavItem>
-                <Nav.Link eventKey='fifth' className="nav-link" style={{ textAlign: 'left' }}>
-                  Libreries
-                </Nav.Link>
-              </NavItem>
-            </Nav>
-          </Col>
-          <Col sm={10}>
-            <TabContent>
-              <TabPane eventKey='first'>
-                <img
-                  src={fotoDisigner}
-                  className='img-fluid'
-                  alt='fotoDisigner'
-                />
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Corporis architecto ipsam libero eligendi qui officiis minus eveniet
-                  accusantium, maxime enim quae laudantium itaque repudiandae.
-                  Eligendi omnis repudiandae ducimus fugit velit?
-                </p>
-              </TabPane>
-              <TabPane eventKey='second'>
-                <img
-                  src={fotoDisigner2}
-                  className='img-fluid'
-                  alt='fotoDisigner2'
-                />
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Corporis architecto ipsam libero eligendi qui officiis minus eveniet
-                  accusantium, maxime enim quae laudantium itaque repudiandae.
-                  Eligendi omnis repudiandae ducimus fugit velit?
-                </p>
-              </TabPane>
-              <TabPane eventKey='third'>
-                <img
-                  src={fotoDisigner3}
-                  className='img-fluid'
-                  alt='fotoDisigner'
-                />
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Corporis architecto ipsam libero eligendi qui officiis minus eveniet
-                  accusantium, maxime enim quae laudantium itaque repudiandae.
-                  Eligendi omnis repudiandae ducimus fugit velit?
-                </p>
-              </TabPane>
-              <TabPane eventKey='fourth'>
-                <img
-                  src={fotoDisigner4}
-                  className='img-fluid'
-                  alt='fotoDisigner'
-                />
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Corporis architecto ipsam libero eligendi qui officiis minus eveniet
-                  accusantium, maxime enim quae laudantium itaque repudiandae.
-                  Eligendi omnis repudiandae ducimus fugit velit?
-                </p>
-              </TabPane>
-              <TabPane eventKey='fifth'>
-                <img
-                  src={fotoDisigner5}
-                  className='img-fluid'
-                  alt='fotoDisigner'
-                />
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Corporis architecto ipsam libero eligendi qui officiis minus eveniet
-                  accusantium, maxime enim quae laudantium itaque repudiandae.
-                  Eligendi omnis repudiandae ducimus fugit velit?
-                </p>
-              </TabPane>
-            </TabContent>
-          </Col>
-        </Row>
-      </TabContainer>
-    </>
-  );
+    return (
+        <>
+            <TabContainer id='ledt-tabs-example' defaultActiveKey='first'>
+                <Row>
+                    <Col sm={2}>
+                        <Nav variant='pills' className='flex-column mt-2'>
+                            <NavItem>
+                                <Nav.Link eventKey='first' className="nav-link" style={{ textAlign: 'left' }}>
+                                    Add Employee
+                                </Nav.Link>
+                            </NavItem>
+                            <NavItem>
+                                <Nav.Link eventKey='second' className="nav-link" style={{ textAlign: 'left' }}>
+                                    Add Unit
+                                </Nav.Link>
+                            </NavItem>
+                            <NavItem>
+                                <Nav.Link eventKey='third' className="nav-link" style={{ textAlign: 'left' }}>
+                                    Add Store
+                                </Nav.Link>
+                            </NavItem>
+                            <NavItem>
+                                <Nav.Link eventKey='fourth' className="nav-link" style={{ textAlign: 'left' }}>
+                                    Add CashExpense
+                                </Nav.Link>
+                            </NavItem>
+                            <NavItem>
+                                <Nav.Link eventKey='fifth' className="nav-link" style={{ textAlign: 'left' }}>
+                                    Add CashIncome
+                                </Nav.Link>
+                            </NavItem>
+                        </Nav>
+                    </Col>
+                    <Col sm={10}>
+                        <TabContent>
+                            <TabPane eventKey='first'>
+                                <img
+                                    src={fotoDisigner}
+                                    className='img-fluid'
+                                    alt='fotoDisigner'
+                                />
+                            </TabPane>
+                            <TabPane eventKey='second'>
+                            </TabPane>
+                            <TabPane eventKey='third'>
+                                <img
+                                    src={fotoDisigner3}
+                                    className='img-fluid'
+                                    alt='fotoDisigner'
+                                />
+                            </TabPane>
+                            <TabPane eventKey='fourth'>
+                                <CashExpense />
+                            </TabPane>
+                            <TabPane eventKey='fifth'>
+                                <img
+                                    src={fotoDisigner5}
+                                    className='img-fluid'
+                                    alt='fotoDisigner'
+                                />
+                            </TabPane>
+                        </TabContent>
+                    </Col>
+                </Row>
+            </TabContainer>
+        </>
+    );
 }
 
 export default Documents
