69d1cc3113fc80f581581ed9ac290f06454c3664
olimov Amir <olimov.amir@mail.ru>
2024-03-18 15:07:26 +0300

    fix

--- src/pages/groupProduct/GroupProduct.jsx
+++ src/pages/groupProduct/GroupProduct.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function GroupProduct() {
     // Состояние для хранения списка должностей
@@ -22,12 +23,22 @@ function GroupProduct() {
         fetchGroups();
     }, []);
     return (
-        <Container>
-            <h2>Группа продуктов</h2>
-            <ul>
-                {groups.map(group => (<li key={group.id}>{group.name} </li>))}
-            </ul>
-        </Container>
+        <div>
+            <div className=''>
+                <Button className='m-2' variant="secondary">Add Group</Button>
+                <Button variant="secondary">Add Name Characteristik</Button>
+            </div>
+            <div>
+                <Container>
+                    <h2>Группа продуктов</h2>
+                    <ul>
+                        {groups.map(group => (<li key={group.id}>{group.name} </li>))}
+                    </ul>
+                </Container>
+            </div>
+            <div>
+            </div>
+        </div>
     )
 }
 
