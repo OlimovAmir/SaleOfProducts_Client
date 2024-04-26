e342ca435f7f8e4dac5e4d6cd85c6059a450e2a9
olimov Amir <olimov.amir@mail.ru>
2024-04-26 20:23:51 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import Header from './components/header/Header';
 
 function App() {
   const [isRegistered, setIsRegistered] = useState(false);
-  
+
   const handleRegistrationSuccess = () => {
     setIsRegistered(true);
   };
   return (
-    <div className="App">
+    <div className="App video-background">
+      <video autoPlay loop muted>
+        <source src="your-video.mp4" type="video/mp4" />
+        {/* Добавьте дополнительные форматы, если нужно */}
+        Your browser does not support the video tag.
+      </video>
       <header className="">
         {isRegistered ? <Header /> : <RegistrationForm onRegistrationSuccess={handleRegistrationSuccess} />}
       </header>
--- src/form/RegistrationForm.jsx
+++ src/form/RegistrationForm.jsx
@@ -4,6 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { Button, Container } from 'react-bootstrap';
+import styles from './RegistrationForm.module.css';
 
 
 function RegistrationForm({ onRegistrationSuccess }) {
@@ -40,37 +41,40 @@ function RegistrationForm({ onRegistrationSuccess }) {
         }
     };
     return (
-        <Container className='mt-5'>
-            <Row>
-                <Col xs={6} md={4}>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group as={Row} className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label column sm="2">Username:</Form.Label>
-                            <Col sm="10">
-                                <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-                            </Col>
-                        </Form.Group>
+        <div className={styles.wrapper}>
+            <Container className='mt-5'>
+                <Row>
+                    <Col xs={6} md={4}>
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group as={Row} className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label column sm="2">Username:</Form.Label>
+                                <Col sm="10">
+                                    <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+                                </Col>
+                            </Form.Group>
 
-                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                            <Form.Label column sm="2"> Password: </Form.Label>
-                            <Col sm="10">
-                                <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-                            </Col>
-                        </Form.Group>
+                            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                                <Form.Label column sm="2"> Password: </Form.Label>
+                                <Col sm="10">
+                                    <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                                </Col>
+                            </Form.Group>
 
-                        <Button type="submit">Log in</Button>
-                    </Form>
-                </Col>
-            </Row>
+                            <Button type="submit">Log in</Button>
+                        </Form>
+                    </Col>
+                </Row>
 
 
-            { /* {accessToken && (
+                { /* {accessToken && (
         <div>
            <p>Access Token: {accessToken}</p>
            <p>Refresh Token: {refreshToken}</p>
         </div>
       )} */}
-        </Container>
+            </Container>
+        </div>
+
     )
 }
 
