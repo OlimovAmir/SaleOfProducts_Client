0c71f2aaf77a9d405bb8a1e7c903c359ff968d36
olimov Amir <olimov.amir@mail.ru>
2024-05-12 12:35:36 +0300

    fix bag

--- src/components/ModalNameCharacteristik.jsx
+++ src/components/ModalNameCharacteristik.jsx
@@ -1,6 +1,6 @@
 import { Modal, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ModalNameCharacteristik({ show, handleClose, groupId, groupName }) {
   const [charactergroupName, setGroupName] = useState('');
@@ -8,10 +8,10 @@ function ModalNameCharacteristik({ show, handleClose, groupId, groupName }) {
   const handleUpdateGroup = async () => {
     try {
       const data = {
-        name: groupName,        
+        name: groupName,
         nameCharacteristicProducts: [
           {
-            name: charactergroupName,            
+            name: charactergroupName,
           }
         ]
       };
@@ -22,14 +22,42 @@ function ModalNameCharacteristik({ show, handleClose, groupId, groupName }) {
       handleClose();
     } catch (error) {
       console.error('Error sending data:', error);
-      
+
       alert('An error occurred while sending data to the server');
       handleClose();
     }
   };
 
+  // раздел поиск и ввод данных из базы 
+  // Состояние для хранения списка 
+  const [characteristics, setCharacteristics] = useState([]);
+
+  // Загрузка списка должностей при монтировании компонента
+  useEffect(() => {
+    // Запрос к API для получения списка 
+    axios.get('http://localhost:5134/NameCharacteristicProduct/AllItems')
+      .then(response => {
+        // Установка списка в состояние
+        setCharacteristics(response.data);
+      })
+      .catch(error => {
+        console.error('Не удалось загрузить список Характеристики:', error);
+      });
+  }, []);
+
+  const [formData, setFormData] = useState({
+    name: '',
+    positionId: '', // Идентификатор должности
+  });
+
+  // Обработчик события для обновления данных формы при изменении ввода
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
-    <Modal show={show} onHide={handleClose}> 
+    <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Update Name Characteristic Product</Modal.Title>
       </Modal.Header>
@@ -42,6 +70,22 @@ function ModalNameCharacteristik({ show, handleClose, groupId, groupName }) {
             value={charactergroupName}
             onChange={(e) => setGroupName(e.target.value)}
           />
+
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPositionId">
+          <Form.Label className='text-left'>Characteristics</Form.Label>
+          
+          <Form.Control
+            as="select"
+            name="positionId"
+            value={formData.positionId}
+            onChange={handleInputChange}
+          >
+            <option value="">Select Characteristics</option>
+            {characteristics && characteristics.map(characteristic => (
+              <option key={characteristic.id} value={characteristic.id}>{characteristic.name}</option>
+            ))}
+          </Form.Control>
         </Form.Group>
       </Modal.Body>
       <Modal.Footer>
@@ -49,7 +93,7 @@ function ModalNameCharacteristik({ show, handleClose, groupId, groupName }) {
           Close
         </Button>
         <Button variant="primary" onClick={handleUpdateGroup}>
-          Update 
+          Update
         </Button>
       </Modal.Footer>
     </Modal>
