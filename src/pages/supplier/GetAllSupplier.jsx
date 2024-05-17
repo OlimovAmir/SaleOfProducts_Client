1da1672c1dcd1ab37b09e0f7385ad9cd84ab3a20
olimov Amir <olimov.amir@mail.ru>
2024-05-17 21:41:58 +0300

    fix bag

--- src/pages/supplier/GetAllSupplier.jsx
+++ src/pages/supplier/GetAllSupplier.jsx
@@ -3,9 +3,48 @@ import { Container } from 'react-bootstrap';
 import axios from 'axios';
 
 function GetAllSupplier() {
-  return (
-    <div>GetAllSupplier</div>
-  )
+    // Состояние для хранения списка Suppliers
+    const [products, setProducts] = useState([]);
+
+    // Функция для загрузки данных о Suppliers из базы
+    const fetchProducts = () => {
+        axios.get('http://localhost:5134/Supplier/AllItems')
+            .then(response => {
+                // Устанавливаем полученные данные о Suppliers в состояние
+                setProducts(response.data);
+            })
+            .catch(error => {
+                console.error('Не удалось загрузить список Suppliers:', error);
+            });
+    };
+    // Загрузка списка Suppliers при монтировании компонента
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+    return (
+        <Container>
+            <h2>Список поставщиков</h2>
+            <ol>
+                <div className='row'>
+                    <div className='col-3'> <b>Наименование:</b>   </div>
+                    <div className='col-2'><b>ИНН:</b> </div>
+                    <div className='col-2'><b>Телефон:</b> </div>
+                    <div className='col-2'><b>Адрес:</b> </div>
+                </div>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <div className='row'>
+                            <div className='col-3'> {product.name} </div>
+                            <div className='col-2'>{product.inn}</div>
+                            <div className='col-2'>{product.phone}</div>
+                            <div className='col-2'>{product.address}</div>
+                        </div>
+
+                    </li>
+                ))}
+            </ol>
+        </Container>
+    )
 }
 
 export default GetAllSupplier
