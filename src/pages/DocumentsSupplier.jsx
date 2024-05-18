e4196e5c2a1c17e582f3250b7acd9be6166a774d
olimov Amir <olimov.amir@mail.ru>
2024-05-18 14:08:57 +0300

    fix bag

--- src/pages/DocumentsSupplier.jsx
+++ src/pages/DocumentsSupplier.jsx
@@ -12,12 +12,7 @@ function DocumentsProduct() {
             <TabContainer id='ledt-tabs-example' defaultActiveKey='first'>
                 <Row>
                     <Col sm={2}>
-                        <Nav variant='pills' className='flex-column mt-2'>
-                            <NavItem>
-                                <Nav.Link eventKey='first' className="nav-link" style={{ textAlign: 'left' }}>
-                                    {language === 'en' ? 'New Supplier' : 'Новый поставщик'}
-                                </Nav.Link>
-                            </NavItem>
+                        <Nav variant='pills' className='flex-column mt-2'>                            
                             <NavItem>
                                 <Nav.Link eventKey='getAll' className="nav-link" style={{ textAlign: 'left' }}>
                                     {language === 'en' ? 'Get All Supplier' : 'Список всех поставщиков'}
