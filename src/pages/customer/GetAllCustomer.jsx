62659c748765ec84664479ddf017a0e659aa078d
olimov Amir <olimov.amir@mail.ru>
2024-05-29 20:10:03 +0300

    fix bag

--- src/pages/customer/GetAllCustomer.jsx
+++ src/pages/customer/GetAllCustomer.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import { Button, Container, Form } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileExport, faList, faPrint} from '@fortawesome/free-solid-svg-icons';
 function GetAllCustomer() {
   return (
-    <div>GetAllCustomer!!!</div>
+    <Container className="">
+      <Form.Control
+        type="text"
+        placeholder="Search"
+        className="mr-sm-2 w-50"
+        value=""
+        
+      />
+      <div className="">
+        <h2>Список поставщиков</h2>
+        <Button
+          className='m-2'
+          size="sm"
+          variant="secondary"
+          
+        >
+          <FontAwesomeIcon icon={faList} /> Добавить нового поставщика
+        </Button>
+        <Button className='m-2' size="sm" variant="secondary" >
+          <FontAwesomeIcon icon={faFileExport} /> Экспорт в Excel
+        </Button>
+        <Button className='m-2' size="sm" variant="secondary">
+          <FontAwesomeIcon icon={faPrint} /> Печать
+        </Button>
+      </div>
+
+      
+      
+      
+    </Container>
   )
 }
 
