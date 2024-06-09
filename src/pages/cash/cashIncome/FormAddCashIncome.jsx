f95c304014d38e07e46291bedd5bd89d2e2cb023
olimov Amir <olimov.amir@mail.ru>
2024-06-09 16:49:57 +0300

    has been modified form addCash

--- src/pages/cash/cashIncome/FormAddCashIncome.jsx
+++ src/pages/cash/cashIncome/FormAddCashIncome.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { closeModalAddCashIncome } from '../../../redux/reducers/modalAddCashIncomeSlice.js'; // Измените на правильный файл
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { Form } from 'react-bootstrap';
 
 function FormAddCashIncome() {
   const dispatch = useDispatch();
@@ -15,10 +16,56 @@ function FormAddCashIncome() {
   return (
     <>
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+      <br/>
+        <Form >
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Control
+              type="date"
+              placeholder="Enter date"
+              name="date"
+            />
+          </Form.Group>
+        </Form>
+        <Form >
+          <Form.Group className="mb-3" controlId="formBasicAmount">
+            <Form.Control
+              type="number"
+              placeholder="Enter amount"
+              name="amount"
+            />
+          </Form.Group>
+        </Form>
+        <Form >
+          <Form.Group className="mb-3" controlId="formBasicDescription">
+            <Form.Control
+              type="text"
+              placeholder="Enter description"
+              name="text"
+            />
+          </Form.Group>
+        </Form>
+
+        <Form >
+          <Form.Group className="mb-3" controlId="formBasicFromWhom">
+            <Form.Control
+              type="text"
+              placeholder="Enter fromWhom"
+              name="fromWhom"
+            />
+          </Form.Group>
+        </Form>
+
+        <Form >
+          <Form.Group className="mb-3" controlId="formBasicIncomeItems">
+            <Form.Control
+              type="text"
+              placeholder="Enter incomeItems"
+              name="incomeItems"
+            />
+          </Form.Group>
+        </Form>
+
+
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
