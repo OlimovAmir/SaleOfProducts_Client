bcb42efb54813f32536f761cd6c1ce73aeada380
olimov Amir <olimov.amir@mail.ru>
2024-06-11 14:18:06 +0300

    fix bag

--- src/pages/cash/cashIncome/FormAddCashIncome.jsx
+++ src/pages/cash/cashIncome/FormAddCashIncome.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeModalAddCashIncome } from '../../../redux/reducers/modalAddCashIncomeSlice.js'; // Измените на правильный файл
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { Form } from 'react-bootstrap';
+import axios from 'axios';
 
 function FormAddCashIncome() {
   const dispatch = useDispatch();
@@ -12,6 +13,38 @@ function FormAddCashIncome() {
   const handleClose = () => {
     dispatch(closeModalAddCashIncome());
   };
+
+  // Состояние для хранения данных формы
+  const [formData, setFormData] = useState({
+    amount: 0,
+    description: '',
+    expenseItemId: '', // Идентификатор 
+    transactionDate: '', // Дата
+    fromWhom: '', //  от кого 
+    
+  });
+
+  // Состояние для хранения списка должностей
+  const [incomeItems, setIncomeItems] = useState([]);
+
+  // Обработчик события для обновления данных формы при изменении ввода
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // Загрузка списка должностей при монтировании компонента
+  useEffect(() => {
+    // Запрос к API для получения списка должностей
+    axios.get('http://localhost:5134/IncomeItem/AllItems')
+      .then(response => {
+        // Установка списка должностей в состояние
+        setIncomeItems(response.data);
+      })
+      .catch(error => {
+        console.error('Не удалось загрузить список должностей:', error);
+      });
+  }, []);
 
   return (
     <>
@@ -35,6 +68,21 @@ function FormAddCashIncome() {
             />
           </Form.Group>
         </Form>
+        <Form.Group className="mb-3" controlId="formBasicIncomeItemId">            
+            <Form.Control
+              as="select"
+              name="incomeItemId"
+              value={formData.incomeItemId}
+              onChange={handleInputChange}
+            >
+              <option value="">Select incomeItem</option>
+              {incomeItems.map(incomeItem => (
+                <option key={incomeItem.id} value={incomeItem.id}>{incomeItem.name}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+        
+
         <Form >
           <Form.Group className="mb-3" controlId="formBasicDescription">
             <Form.Control
@@ -55,23 +103,14 @@ function FormAddCashIncome() {
           </Form.Group>
         </Form>
 
-        <Form >
-          <Form.Group className="mb-3" controlId="formBasicIncomeItems">
-            <Form.Control
-              type="text"
-              placeholder="Enter incomeItems"
-              name="incomeItems"
-            />
-          </Form.Group>
-        </Form>
-
+       
 
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Save
           </Button>
         </Modal.Footer>
       </Modal>
